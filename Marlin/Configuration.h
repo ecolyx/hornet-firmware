7fce6d9858cba01b911c12091c70370c026ea677
Charlie Stott <8129891+ecolyx@users.noreply.github.com>
2021-06-06 00:16:09 +1000

    Update Configuration.h
    Add filaments

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1501,15 +1501,25 @@
 // @section temperature
 
 // Preheat Constants
+#define PREHEAT_1_LABEL       "ABS"
+#define PREHEAT_1_TEMP_HOTEND 225
+#define PREHEAT_1_TEMP_BED    100
+#define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
+
 #define PREHEAT_1_LABEL       "PLA"
 #define PREHEAT_1_TEMP_HOTEND 200
-#define PREHEAT_1_TEMP_BED     70
+#define PREHEAT_1_TEMP_BED     80
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "PETG"
 #define PREHEAT_2_TEMP_HOTEND 240
 #define PREHEAT_2_TEMP_BED     80
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
+
+#define PREHEAT_1_LABEL       "Custom"
+#define PREHEAT_1_TEMP_HOTEND 190
+#define PREHEAT_1_TEMP_BED     70
+#define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 /**
  * Nozzle Park
